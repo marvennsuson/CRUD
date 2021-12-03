2f1123f9cb03330e18857382a02f4a799394fe64
marvenn Suson <msuson@highlysucceed.com>
2021-12-03 09:34:52 +0000

    Change Text Title

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,7 +12,7 @@ class ArticleController extends Controller
     public function index(){
         $article = Article::all();
 
-        $page_tile = "INDEX ARTICLESss";
+        $page_tile = "INDEX";
         return view('index',compact('article','page_tile'));
 
     }
